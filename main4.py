a1d4f4ac182606994a833acc720c642acafa6ec3
Pkeshavvardhan <keshav1008podar@gmail.com>
2025-03-21 17:59:19 +0530

    exam eligibility

---
+++ main4.py
@@ -0,0 +1,6 @@
+number=int(input("write down the down the number of you were present for the coarse"))
+
+if number>75:
+    print("then you are eligible for the exam")
+else:
+    print("you are not eligible for the exam")
